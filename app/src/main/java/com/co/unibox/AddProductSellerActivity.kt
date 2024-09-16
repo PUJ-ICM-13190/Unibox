7c081ce25f4cae1c4acb9e053d3a5ecaa03026a7
Camila Neiza <castronjulianac@javeriana.edu.co>
2024-09-16 16:43:29 -0500

    agregar_producto_vendedor cambio 1

---
+++ app/src/main/java/com/co/unibox/AddProductSellerActivity.kt
@@ -0,0 +1,17 @@
+package com.co.unibox
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class AddProductSellerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_add_product_seller)
+
+    }
+
+}
